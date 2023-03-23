09f090724267a395ef45ded172b053e0fe6d2625
tnakamura <griefworker@gmail.com>
2023-03-23 09:31:04 +0900

    Turbo Drive を試した。

---
+++ examples/WireFrame/TagHelpers/TurboFrameTagHelper.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Hotwire.AspNetCore.TagHelpers;
+
+public class TurboFrameTagHelper : TagHelper
+{
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        output.TagName = "turbo-frame";
+    }
+}
