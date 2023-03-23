219617af5f780ba19c6e0ac34399b4e495ed03db
tnakamura <griefworker@gmail.com>
2023-03-23 16:42:23 +0900

    Turbo Framew を試した。

---
+++ examples/WireFrame/Controllers/GalleryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WireFrame.Controllers
+{
+    public class GalleryController : Controller
+    {
+        public IActionResult Forest()
+        {
+            return View();
+        }
+
+        public IActionResult Mountains()
+        {
+            return View();
+        }
+
+        public IActionResult Ocean()
+        {
+            return View();
+        }
+    }
+}
