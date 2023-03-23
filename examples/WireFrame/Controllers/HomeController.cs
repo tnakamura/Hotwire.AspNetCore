042cbd1a0a9b9390e6e7e9b1379083c3732d3abf
tnakamura <griefworker@gmail.com>
2023-03-23 16:23:45 +0900

    Turbo Drive のサンプル変更。

--- examples/WireFrame/Controllers/HomeController.cs
+++ examples/WireFrame/Controllers/HomeController.cs
@@ -18,7 +18,12 @@ namespace WireFrame.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult About()
         {
             return View();
         }
