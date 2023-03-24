bc51e1cacb2d471a79f17ea24ff2f62cb354b679
tnakamura <griefworker@gmail.com>
2023-03-24 15:55:12 +0900

    WireStream プロジェクトを追加。

---
+++ examples/WireStream/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WireStream.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
