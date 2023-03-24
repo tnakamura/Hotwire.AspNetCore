6021253f9e93c9c049336650301c6668053049e8
tnakamura <griefworker@gmail.com>
2023-03-24 15:17:56 +0900

    TurboStreamTagHelper を追加。

---
+++ src/Hotwire.AspNetCore/TagHelpers/TurboStreamTagHelper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Hotwire.AspNetCore.TagHelpers
+{
+    public class TurboStreamTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "turbo-stream";
+            output.PreContent.SetHtmlContent("<template>");
+            output.PostContent.SetHtmlContent("</template>");
+        }
+    }
+}
