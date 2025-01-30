e4f5d1abd9e08ca835ce21d5b52cd5271b6aa9ca
tnakamura <griefworker@gmail.com>
2025-01-30 10:37:16 +0900

    Add TurboHttpRequestExtensions

---
+++ src/Turbo.AspNetCore/TurboHttpRequestExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Http;
+
+namespace Turbo.AspNetCore
+{
+    public static class TurboHttpRequestExtensions
+    {
+        public static bool IsTurboFrameRequest(this HttpRequest request)
+        {
+            return request.Headers.Any(x => x.Key == "turbo-frame");
+        }
+
+        public static bool IsTurboStreamRequest(this HttpRequest request)
+        {
+            return request.GetTypedHeaders().Accept.Any(x => x.MediaType == "text/vnd.turbo-stream.html");
+        }
+    }
+}
